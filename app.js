3d3f2124789c75c332e7e99aa2589e29db0db09d
AidanCollar <300088221@fvtc.edu>
2024-04-23 20:11:35 -0500

    fixed the static route

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
+app.use(express.static('public'))
 app.use('/api', require('./routes/api-v1'))
+app.use('/',require('./routes/static'))
 
 app.listen(port, () => console.log(`Server is running http://localhost:${port}`))
