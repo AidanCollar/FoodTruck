c52e1b1d301da953c939a598274e1998a9df1eae
AidanCollar <300088221@fvtc.edu>
2024-05-07 19:05:06 -0500

    added the events to the home

--- public/scripts/site.js
+++ public/scripts/site.js
@@ -22,6 +22,32 @@ const displayMenu = async () => {
     })
 }
 
+const displayEvents=async()=>{
+    const response = await fetch('/api/event')
+    const event = await response.json() 
+    const unorderedlist = document.getElementById('events')
+    event.forEach(item=>{
+    const li2 = document.createElement('li')
+    const ul = document.createElement('ul')
+    const li = document.createElement('li')
+    li.textContent="Event Name :"+item.name 
+    unorderedlist.appendChild(li)
+    
+    
+    li2.textContent+="Location: "+item.location
+    ul.appendChild(li2)
+    li3 = document.createElement('li')
+    li3.textContent+="Date: "+item.date
+    ul.appendChild(li3) 
+    li4 = document.createElement('li')
+    li4.textContent+="Hours: "+item.hours
+    ul.appendChild(li4)
+    unorderedlist.appendChild(ul)
 
 
+   
+    })
+    
+}
+displayEvents()
 displayMenu()
